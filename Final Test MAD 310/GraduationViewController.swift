b3fb757e2df6020eb955f0899756fcb40ff69476
Jesse viau <jesseviau@Jesses-MacBook-Air.local>
2021-12-16 18:42:53 -0500

    added UIImageView

---
+++ Final Test MAD 310/GraduationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GraduationViewController.swift
+//  Final Test MAD 310
+//
+//  Created by Jesse viau on 2021-12-16.
+//
+
+import UIKit
+
+class GraduationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
