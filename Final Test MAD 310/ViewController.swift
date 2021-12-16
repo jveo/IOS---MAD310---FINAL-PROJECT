8b27c658abe44ebbbcfd80631954ba66ecc53f34
Jesse viau <jesseviau@Jesses-MacBook-Air.local>
2021-12-16 18:19:07 -0500

    Implemented second 'MAD Grad' controller

--- Final Test MAD 310/ViewController.swift
+++ Final Test MAD 310/ViewController.swift
@@ -13,8 +13,16 @@ class ViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
+        /*
+        
+         const urlString is the json location of the data
+         
+         if that url string is valid
+         proceed ro parse the contents of the json url with the parseJson() function below
+         
+         
+         */
         let urlString = "https://dtakaki.scweb.ca/mad310/version3Data.json"
         
         if let url = URL(string: urlString){
@@ -24,19 +32,31 @@ class ViewController: UITableViewController {
         }
     }
 
+    
     func parseJson(json: Data){
+        
+        //intantiate the decoder
         let decoder = JSONDecoder()
         
+        // if jsonCompaies == true then proceed to make the companies array the parsed json results
         if let jsonCompanies = try? decoder.decode(Companies.self, from:json){
             companies = jsonCompanies.results
             tableView.reloadData()
         }
     }
     
+    
+    //returning the number of results
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return companies.count
     }
     
+    
+    /* tableView function returning a UITableViewCell to display the information
+     
+     
+     
+     */
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         let company = companies[indexPath.row]
